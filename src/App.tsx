9dedc16b238b95384ce863ec32c6d2ee60da2a56
Naim Chowdhury <Naim.91@hotmail.com>
2023-12-11 14:44:53 +0100

    [feature] Added Embla Carousel to the webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
+import Carousel from "./components/Carousel/Carousel"
 
+
+const OPTIONS = {};
+const SLIDE_COUNT = 6;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 function App() {
 
 
   return (
     <>
       <Navbar />
+      <div className="sandbox app__bg">
+        <section className="sandbox__carousel">
+          <Carousel slides={SLIDES} options={OPTIONS} />
+        </section>
+      </div>
       <h1 className="text-3xl font-bold underline text-center text-white">
       Hello world!
       </h1>
---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,33 @@
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+import { images } from "../../data/CarouselData";
+import "./Carousel.css";
+
+const EmblaCarousel = (props) => {
+  const { slides, options } = props;
+  const [emblaRef] = useEmblaCarousel(options, [Autoplay()]);
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index: number) => {
+            const imageData = images[index % images.length];
+            return (
+              <div className="embla__slide" key={index}>
+                <img
+                  className="embla__slide__img"
+                  src={imageData.src}
+                  alt={imageData.alt}
+                />
+                <p className="embla__text p__cormorant">{imageData.alt}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmblaCarousel;
