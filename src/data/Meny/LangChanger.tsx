e2ac385cc0790f7bb25aa648b0027a529734f8b2
Naim Chowdhury <Naim.91@hotmail.com>
2024-01-22 01:27:10 +0100

    Starting language changer component

---
+++ src/data/Meny/LangChanger.tsx
@@ -0,0 +1,8 @@
+// LangChanger.tsx
+
+import { Sushi as SushiSwe } from "./SushiSwedish";
+import { Sushi as SushiEng } from "./SushiEnglish";
+
+
+export const SwedishMeny: MenuCategory[] = [SushiSwe];
+export const EnglishMeny: MenuCategory[] = [SushiEng];
